41981d21c78c4e707253450cae014d8279f5a08b
coolbreeze9 <coolbreeze9@proton.me>
2026-05-26 01:21:06 +0200

    Mejorar script

--- start-lab.sh
+++ start-lab.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 set -e
 
-echo "[1] Arrancando contenedores."
+echo "[1] Arrancando contenedores"
 docker compose up -d
 
-echo "[2] Esperando red Docker."
-sleep 8
+echo "[2] Esperando red Docker"
+until ip link show | grep -q 'br-[a-f0-9]*'; do
+  sleep 1
+done
 
-echo "[3] Detectando interfaz bridge."
+echo "[3] Detectando interfaz bridge"
 IFACE=$(ip link show | grep -o 'br-[a-f0-9]*' | head -1)
 
 if [ -z "$IFACE" ]; then
-  echo "ERROR: Interfaz no encontrada. Verifica Docker."
+  echo "ERROR: Interfaz no encontrada"
   exit 1
 fi
 
 echo "[4] Configurando Suricata: $IFACE"
 sed -i "s/- interface: br-.*/- interface: $IFACE/" ~/security-lab/suricata/config/suricata.yaml
 
-echo "[5] Reiniciando Suricata."
+echo "[5] Reiniciando Suricata"
 docker compose restart suricata-ips
 
 echo "Laboratorio listo"
